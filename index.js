50a355569548e345756d4d6044ec49dabd7ff672
Jean Cochrane <jean@jeancochrane.com>
2018-10-29 15:37:39 -0400

    Textbox now makes predictions from a model + weights

--- index.js
+++ index.js
@@ -5,11 +5,35 @@ const doc = document;  // eslint-disable-line no-undef
 var textarea = doc.getElementById('textarea');
 const wordButtons = doc.getElementsByClassName('word-button');
 
-let vocab = [
+let initialVocab = [
     'dolores', 'placeat', 'culpa', 'laborum', 'natus', 'dignissimos', 'et', 'ut',
     'omnis', 'rerum', 'quas', 'exercitationem', 'tempore', 'repellat', 'enim',
     'soluta', 'error', 'deleniti', 'est', 'perspiciatis'
 ];
+
+class Vocab {
+    constructor(words) {
+        this.words = words;
+        this.wordToIdx = this.words.reduce((obj, word, idx) => {
+            obj[word] = idx;
+            return obj;
+        }, {});
+    }
+
+    get length() {
+        return this.words.length;
+    }
+
+    toIdx(word) {
+        return this.wordToIdx[word];
+    }
+
+    toWord(idx) {
+        return this.words[idx];
+    }
+}
+
+let vocab = new Vocab(initialVocab);
 
 let modelFile = null;
 let weightsFiles = null;
@@ -22,20 +46,19 @@ function handleTextUpload(e, callback) {
 }
 
 function onVocabLoad(e) {
-    vocab = JSON.parse(e.target.result).vocab;
+    vocab = new Vocab(JSON.parse(e.target.result).vocab);
 }
 
 doc.getElementById('vocab-upload').addEventListener('change', e => { handleTextUpload(e, onVocabLoad) }, false);
 doc.getElementById('json-upload').addEventListener('change', e => { modelFile = e.target.files[0] }, false);
 doc.getElementById('weights-upload').addEventListener('change', e => {
     weightsFiles = e.target.files;
-    for (let file of weightsFiles) {
+    tf.loadModel(tf.io.browserFiles([modelFile, ...weightsFiles]))
+      .then(loadedModel => {
+        model = loadedModel;
         // eslint-disable-next-line no-undef
-        console.log(file.name); // eslint-disable-line no-console
-    }
-    // eslint-disable-next-line no-undef
-    console.log(weightsFiles); // eslint-disable-line no-console
-    model = tf.loadModel(tf.io.browserFiles([modelFile, ...weightsFiles]));
+        console.log('Model loaded!'); // eslint-disable-line no-console
+      });
 });
 
 function addFullWord(word) {
@@ -43,20 +66,30 @@ function addFullWord(word) {
 }
 
 function getRandomWord(vocab) {
+    // eslint-disable-next-line no-undef
+    console.log(); // eslint-disable-line no-console
     let randomIndex = Math.floor(Math.random() * vocab.length);
-    let randomWord = vocab.splice(randomIndex, 1)[0];  // `splice` returns an array
+    let randomWord = vocab.words.splice(randomIndex, 1)[0];  // `splice` returns an array
     return randomWord;
 }
 
 function getPredictedWord(vocab, model) {
     const prevInput = textarea.value.split(/\s+/);  // match any number of whitespace-like chars
     const hasPrevInput = !(prevInput.length === 1 && prevInput[0] === '');
-    const predictedIdx = (hasPrevInput) ? model.predict(tf.tensor(prevInput)) : getRandomWord(vocab);
-    return vocab[predictedIdx];  // use the predicted index to find the predicted word
+    let predictedIdx = 0;
+    if (hasPrevInput) {
+        const encodedPrevInput = prevInput.map(word => vocab.toIdx(word));
+        const inputVector = tf.oneHot(tf.tensor1d(encodedPrevInput, 'int32'), vocab.length).expandDims(0);
+        const predictedLogits = model.predict(inputVector);
+        predictedIdx = tf.multinomial(predictedLogits.flatten(), 1).dataSync()[0];
+    } else {
+        predictedIdx = getRandomWord(vocab);
+    }
+    return vocab.toWord(predictedIdx);  // use the predicted index to find the predicted word
 }
 
 function updateRecs() {
-    let vocabCopy = vocab.slice();
+    let vocabCopy = new Vocab(vocab.words.slice());
     const getWord = (model === null) ? getRandomWord : getPredictedWord;
 
     for (let button of wordButtons) {
